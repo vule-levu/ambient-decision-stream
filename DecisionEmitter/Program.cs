c0fe2f30018df214ec69658e85ac83fa40223961
Vu Le <vle@quicksuitetrading.com>
2025-12-23 18:23:54 +0700

    Introduce temporal distortion in decision events

--- DecisionEmitter/Program.cs
+++ DecisionEmitter/Program.cs
@@ -1,21 +1,25 @@
 ﻿var random = new Random();
 var decisionId = "volume-setting";
+var events = new List<DecisionChanged>();
 
 for (var i = 1; i <= 5; i++)
 {
     var from = random.Next(0, 3);
     var to = random.Next(0, 3);
 
-    var evt = new DecisionChanged(
+    events.Add(new DecisionChanged(
         decisionId,
         "user-1",
         "level",
         from,
         to,
         i,
-        DateTime.UtcNow
-    );
+        DateTime.UtcNow.AddSeconds(random.Next(-20, 5))
+    ));
+}
 
+foreach (var evt in events.OrderBy(_ => random.Next()))
+{
     Console.WriteLine(evt);
-    await Task.Delay(500);
+    await Task.Delay(random.Next(200, 800));
 }
